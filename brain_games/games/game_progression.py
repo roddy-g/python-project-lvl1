81367b38ae0e885d8785bbf81524a6804e531ed7
Roddy <per677199@yandex.ru>
2020-09-24 10:07:08 +0300

    fixed linter mistakes

--- brain_games/games/game_progression.py
+++ brain_games/games/game_progression.py
@@ -1,15 +1,14 @@
 from brain_games.scripts import game_flow
-from brain_games.scripts import gcd
 import random
 import prompt
 
 
 def main(name):
     for _ in range(3):
-        step = random.randint(1,10)
-        start = random.randint(1,50)
+        step = random.randint(1, 10)
+        start = random.randint(1, 50)
         row = [(start + step * i) for i in range(10)]
-        missing_place = random.randint(0,9)
+        missing_place = random.randint(0, 9)
         correct_answer = row[missing_place]
         row[missing_place] = ".."
         row_string = ' '.join([str(elem) for elem in row])
