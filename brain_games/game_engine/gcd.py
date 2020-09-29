132efb7012c95f9a7294677a6a5521abe0d67ca2
Roddy <per677199@yandex.ru>
2020-09-29 22:06:03 +0300

    relocated gcd function to gcd.py, deleted magic numbers

--- brain_games/game_engine/gcd.py
+++
@@ -1,8 +0,0 @@
-def gcd(number_1, number_2):
-    while number_1 != 0 and number_2 != 0:
-        if number_1 > number_2:
-            number_1 %= number_2
-        else:
-            number_2 %= number_1
-    result = number_1 + number_2
-    return result
--- brain_games/games/gcd.py
+++ brain_games/games/gcd.py
@@ -1,10 +1,13 @@
-from brain_games.game_engine.gcd import gcd
 import random
 
 
+NUMBER_LOWER_LIMIT = 1
+NUMBER_UPPER_LIMIT = 100
+
+
 def play():
-    number_1 = random.randint(1, 100)
-    number_2 = random.randint(1, 100)
+    number_1 = random.randint(NUMBER_LOWER_LIMIT, NUMBER_UPPER_LIMIT)
+    number_2 = random.randint(NUMBER_LOWER_LIMIT, NUMBER_UPPER_LIMIT)
     question = str(number_1) + ' ' + str(number_2)
     correct_answer = gcd(number_1, number_2)
     return correct_answer, question
@@ -12,3 +15,13 @@ def play():
 
 def print_game_task():
     print("Find the greatest common divisor of given numbers.\n")
+
+
+def gcd(number_1, number_2):
+    while number_1 != 0 and number_2 != 0:
+        if number_1 > number_2:
+            number_1 %= number_2
+        else:
+            number_2 %= number_1
+    result = number_1 + number_2
+    return result
