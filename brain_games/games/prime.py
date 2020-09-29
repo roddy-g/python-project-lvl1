6f41b908549153fdd079746284d46f159359e2d2
Roddy <per677199@yandex.ru>
2020-09-29 22:09:43 +0300

    relocated prime function, deleted magic numbers

--- brain_games/games/prime.py
+++ brain_games/games/prime.py
@@ -1,9 +1,12 @@
-from brain_games.game_engine.is_prime import is_prime
 import random
 
 
+NUMBER_LOWER_LIMIT = 1
+NUMBER_UPPER_LIMIT = 30
+
+
 def play():
-    number = random.randint(1, 30)
+    number = random.randint(NUMBER_LOWER_LIMIT, NUMBER_UPPER_LIMIT)
     if is_prime(number):
         correct_answer = 'yes'
     else:
@@ -13,3 +16,13 @@ def play():
 
 def print_game_task():
     print("Answer \"yes\" if number is prime. Otherwise answer \"no\".\n")
+
+
+def is_prime(number):
+    if number > 1:
+        for i in range(2, number):
+            if (number % i) == 0:
+                return False
+    else:
+        return False
+    return True
