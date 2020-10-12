a3d79998fa8c9c53ab37e18a227ed610974f4915
Roddy <per677199@yandex.ru>
2020-10-12 20:11:28 +0300

    deleted flag from gameflow module

--- brain_games/game_engine/game_flow.py
+++ brain_games/game_engine/game_flow.py
@@ -1,7 +1,7 @@
 import prompt
 
 
-NUMBER_OF_GAME_CYCLES = 3
+NUMBER_OF_GAME_ROUNDS = 3
 
 
 def get_name_and_greet():
@@ -14,17 +14,15 @@ def play_game(game):
     print("\nWelcome to the Brain Games!")
     print(game.GAME_TASK)
     user_name = get_name_and_greet()
-    for _ in range(NUMBER_OF_GAME_CYCLES):
+    for round_ in range(NUMBER_OF_GAME_ROUNDS):
         correct_answer, question = game.generate_round()
         answer = prompt.string("Question: {}\n".format(question))
         if answer == correct_answer:
             print('Correct!')
-            all_anwers_were_correct = True
+            if (round_+1) == NUMBER_OF_GAME_ROUNDS:
+                print('Congratulations, {}!'.format(user_name))
         else:
             print('\"{}\" is wrong answer ;(. '.format(answer), end='')
             print('Correct answer was \"{}\"'.format(correct_answer))
             print('Let\"s try again, {}!'.format(user_name))
-            all_anwers_were_correct = False
             break
-    if all_anwers_were_correct:
-        print('Congratulations, {}!'.format(user_name))
