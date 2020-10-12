918b80f0ed59a80b0217049ec71bd6a7424e8477
Roddy <per677199@yandex.ru>
2020-10-12 21:01:36 +0300

    fixed possible IndexError in missing place index

--- brain_games/games/progression.py
+++ brain_games/games/progression.py
@@ -7,7 +7,6 @@ PROGRESSION_START_MIN = 1
 PROGRESSION_START_MAX = 50
 PROGRESSION_LENGTH = 10
 MISSING_PLACE_MIN = 0
-MISSING_PLACE_MAX = 9
 GAME_TASK = 'What number is missing in the progression?\n'
 
 
@@ -15,7 +14,7 @@ def generate_round():
     step = random.randint(STEP_MIN, STEP_MAX)
     start = random.randint(PROGRESSION_START_MIN, PROGRESSION_START_MAX)
     progression = [(start + step * i) for i in range(PROGRESSION_LENGTH)]
-    missing_place = random.randint(MISSING_PLACE_MIN, MISSING_PLACE_MAX)
+    missing_place = random.randint(MISSING_PLACE_MIN, PROGRESSION_LENGTH - 1)
     correct_answer = str(progression[missing_place])
     progression[missing_place] = ".."
     question = ' '.join([str(elem) for elem in progression])
