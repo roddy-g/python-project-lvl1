4ca1b7caa7504c58043939cb62ed7055ee34ef78
Roddy <per677199@yandex.ru>
2020-10-13 21:14:48 +0300

    moved math logic to function

--- brain_games/games/even.py
+++ brain_games/games/even.py
@@ -8,8 +8,15 @@ GAME_TASK = 'Answer "yes" if number even otherwise answer "no".\n'
 
 def generate_round():
     number = random.randint(NUMBER_LOWER_LIMIT, NUMBER_UPPER_LIMIT)
-    if number % 2 == 0:
+    if is_even(number):
         correct_answer = 'yes'
     else:
         correct_answer = 'no'
     return correct_answer, number
+
+
+def is_even(number):
+	if number % 2 == 0:
+		return True
+	else:
+		return False
